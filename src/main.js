28672d31d49aedbc424d82b688e1bc4838d7afe7
Carlos <carlosaac232001@gmail.com>
2025-10-07 15:13:41 -0500

    feat: format all code with biome and use formatIconName function

--- src/main.js
+++ src/main.js
@@ -1,32 +1,58 @@
-const fetchBtn = document
-  .getElementById('fetchBtn')
-  .addEventListener('click', getWeatherData);
+import { formatIconName } from './utils/domUtils';
+import './styles/reset.css';
+import './styles/style.css';
+
+const fetchBtn = document.querySelector('.container__btn');
+fetchBtn.addEventListener('click', getWeatherData);
 
 async function getWeatherData() {
-  const location = document
-    .getElementById('location')
-    .value.toLowerCase()
-    .trim();
-  const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${process.env.VISUAL_CROSSING_API_KEY}`;
+	const location = document
+		.getElementById('location')
+		.value.toLowerCase()
+		.trim();
+	const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${process.env.VISUAL_CROSSING_API_KEY}`;
 
-  try {
-    const res = await fetch(URL);
-    const data = await res.json();
-    processData(data);
-  } catch (error) {
-    alert(error);
-  }
+	try {
+		const res = await fetch(URL);
+		const data = await res.json();
+		processWeatherData(data);
+	} catch (error) {
+		alert(error);
+	}
 }
 
-function processData(data) {
-  const { address: city, description, timezone } = data;
-  const { conditions, icon, temp } = data.currentConditions;
+async function getWeatherGif(conditions) {
+	const URL = `https://api.giphy.com/v1/stickers/translate?api_key=${process.env.GIPHY_API_KEY}&s=${conditions}`;
 
-  renderData({ city, description, timezone, conditions, icon, temp });
+	try {
+		const res = await fetch(URL);
+		const data = await res.json();
+		renderWeatherGif(data);
+	} catch (error) {
+		alert(error);
+	}
 }
 
-function renderData(data) {
-  console.log(data);
-  const { city, description, timezone, conditions, icon, temp } = data;
-  const iconURL = `https://github.com/visualcrossing/WeatherIcons/raw/main/SVG/2nd%20Set%20-%20Color/${icon}.svg`;
+function renderWeatherGif(data) {
+	const { url } = data.data.images.original;
+	const img = document.createElement('img');
+	img.src = url;
+
+	document.body.appendChild(img);
+}
+
+function processWeatherData(data) {
+	const { address: city, description, timezone } = data;
+	const { conditions, icon, temp } = data.currentConditions;
+
+	renderWeatherData({ city, description, timezone, conditions, icon, temp });
+}
+
+function renderWeatherData(data) {
+	console.log(data);
+	const { city, description, timezone, conditions, icon, temp } = data;
+	const gifConditions = formatIconName(icon);
+	console.log(gifConditions);
+	getWeatherGif(conditions);
+	const iconURL = `https://github.com/visualcrossing/WeatherIcons/raw/main/SVG/2nd%20Set%20-%20Color/${icon}.svg`;
 }
