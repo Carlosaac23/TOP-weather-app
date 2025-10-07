a5aa09b661d2b0dffd07e52611cc6f6804e354e2
Carlos <carlosaac232001@gmail.com>
2025-10-07 10:55:57 -0500

    chore: improve naming functions

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 const fetchBtn = document
   .getElementById('fetchBtn')
-  .addEventListener('click', fetchWeather);
+  .addEventListener('click', getWeatherData);
 
-async function fetchWeather() {
+async function getWeatherData() {
   const location = document
     .getElementById('location')
     .value.toLowerCase()
     .trim();
   const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${process.env.VISUAL_CROSSING_API_KEY}`;
+
   try {
     const res = await fetch(URL);
     const data = await res.json();
