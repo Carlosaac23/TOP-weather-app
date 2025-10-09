3ef59b48fe784cc67b4994164772c800f063bd5f
Carlos <carlosaac232001@gmail.com>
2025-10-08 20:15:07 -0500

    feat: - change formatIconName name to formatWords to general use - create fahrenheitToCelcius function - create capitalize function to capitalize all cities in card - create formatTimezone to prevent timezones with underscore character

--- src/utils/domUtils.js
+++ src/utils/domUtils.js
@@ -1,3 +1,15 @@
-export function formatIconName(iconName) {
-	return iconName.split("-").join(" ");
+export function formatWords(words) {
+	return words.split('-').join(' ');
+}
+
+export function fahrenheitToCelcius(fahrenheit) {
+	return (((fahrenheit - 32) * 5) / 9).toFixed(0);
+}
+
+export function capitalize(word) {
+	return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+export function formatTimezone(timezone) {
+	return timezone.replace('_', ' ');
 }
